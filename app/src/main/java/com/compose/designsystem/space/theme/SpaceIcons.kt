15308f07a75da24e17e9a54a3ec360aa709a711a
semyon <s.zadorozhnyi>
2022-09-01 06:29:12 +0400

    (Step 9) Provide icons as composables

---
+++ app/src/main/java/com/compose/designsystem/space/theme/SpaceIcons.kt
@@ -0,0 +1,150 @@
+package com.compose.designsystem.space.theme
+
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.compose.designsystem.R
+
+object SpaceIcons
+
+@Composable
+fun SpaceIcons.IcAccessTime(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcAdd(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcApps(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcArrowDropDown(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcAttachMoney(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcCardTravel(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcCheck(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcCheckCircle(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcClose(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
+
+@Composable
+fun SpaceIcons.IcContactSupport(
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = SpaceTheme.colors.shadesBlack,
+) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_access_time),
+        contentDescription = contentDescription,
+        modifier = modifier,
+        tint = tint,
+    )
+}
