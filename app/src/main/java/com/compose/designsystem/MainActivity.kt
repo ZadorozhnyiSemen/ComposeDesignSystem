c69c4c899746d6291277d280ba01f3f4d76ed19b
semyon <s.zadorozhnyi>
2022-09-01 06:15:09 +0400

    (Step 7) Hello world

--- app/src/main/java/com/compose/designsystem/MainActivity.kt
+++ app/src/main/java/com/compose/designsystem/MainActivity.kt
@@ -3,13 +3,12 @@ package com.compose.designsystem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.compose.designsystem.space.atoms.Text
 import com.compose.designsystem.ui.theme.ComposeDesignSystemTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +17,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeDesignSystemTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
                 }
@@ -31,7 +29,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello world!")
 }
 
 @Preview(showBackground = true)
