067a3dfc094a1650131b5df6d9bc1a46aa5e035c
semyon <s.zadorozhnyi>
2022-09-02 05:23:43 +0400

    (Step 12) Add divider and link atoms

---
+++ app/src/main/java/com/compose/designsystem/space/atoms/Divider.kt
@@ -0,0 +1,36 @@
+package com.compose.designsystem.space.atoms
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.compose.designsystem.space.theme.LocalContentColor
+
+@Composable
+fun Divider(
+    modifier: Modifier = Modifier,
+    color: Color = LocalContentColor.current,
+    thickness: Dp = 2.dp,
+    startIndent: Dp = 0.dp
+) {
+    val indentMod = if (startIndent.value != 0f) {
+        Modifier.padding(start = startIndent)
+    } else {
+        Modifier
+    }
+    val targetThickness = if (thickness == Dp.Hairline) {
+        (1f / LocalDensity.current.density).dp
+    } else {
+        thickness
+    }
+    Box(
+        modifier.then(indentMod)
+            .fillMaxWidth()
+            .height(targetThickness)
+            .background(color = color)
+    )
+}
---
+++ app/src/main/java/com/compose/designsystem/space/atoms/Link.kt
@@ -0,0 +1,68 @@
+package com.compose.designsystem.space.atoms
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import com.compose.designsystem.space.theme.SpaceTheme
+
+sealed interface LinkType {
+    object Primary : LinkType
+    object Secondary : LinkType
+    object Danger : LinkType
+}
+
+@Composable
+fun Link(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    drawUnderline: Boolean = true,
+    iconLeft: @Composable (() -> Unit)? = null,
+    iconRight: @Composable (() -> Unit)? = null,
+    type: LinkType = LinkType.Primary,
+    content: @Composable RowScope.() -> Unit,
+) {
+    val color = when(type) {
+        LinkType.Primary -> SpaceTheme.colors.primary_4
+        LinkType.Secondary -> SpaceTheme.colors.neutral_2
+        LinkType.Danger -> SpaceTheme.colors.error_2
+    }
+
+    Surface(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        shape = RectangleShape,
+        contentColor = color
+    ) {
+        ProvideTextStyle(value = SpaceTheme.typography.h4) {
+            Column(
+                modifier = Modifier.width(IntrinsicSize.Max)
+            ) {
+                Row(
+                    modifier = Modifier.padding(
+                        vertical = 8.dp,
+                    ),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    iconLeft?.let {
+                        iconLeft()
+                        Spacer(modifier = Modifier.width(8.dp))
+                    }
+                    content()
+                    iconRight?.let {
+                        Spacer(modifier = Modifier.width(8.dp))
+                        iconRight()
+                    }
+                }
+                if (drawUnderline) {
+                    Divider()
+                }
+            }
+        }
+    }
+}
